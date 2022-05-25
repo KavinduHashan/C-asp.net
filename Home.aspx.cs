9aa989a8cf42cae447d517b832fab4e75d9fb37f
Kavindu Hashan <89184906+KavinduHashan@users.noreply.github.com>
2022-05-25 16:34:06 +0530

    Add files via upload

---
+++ Home.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace login
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox1.Text = class2.First;
+            TextBox2.Text = class2.Fullname;
+            TextBox3.Text = class2.Phone;
+            TextBox4.Text = class2.Vehiclenum;
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            class2.First = TextBox1.Text;
+            class2.Fullname = TextBox2.Text;
+            class2.Phone = TextBox3.Text;
+            class2.Vehiclenum = TextBox4.Text;
+            Response.Redirect("accident.aspx");
+        }
+    }
+}
